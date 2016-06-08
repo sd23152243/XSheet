43786ae4b32b9b90e5f8c97d5deb0ca86a1b86b1
123016 <766812700@qq.com>
2016-06-08 16:17:31 +0800

    Create BasicRange

---
+++ XSheet/Action/AbstractAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using XSheet.Data;
+
+namespace XSheet.Action
+{
+    abstract class AbstractAction
+    {
+        XNamedRange sRange { get; set; }
+        XNamedRange dRange { get; set; }
+        abstract public String doAction(XRange selectedRange);
+        
+
+    }
+}
---
+++ XSheet/Data/XNamedRange.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XSheet.Action;
+
+namespace XSheet.Data
+{
+    class XNamedRange:RangeActionFactory
+    {
+        String rangName { get; set; }
+        List<XRange> ranges { get; set; }
+        Dictionary<string,AbstractAction> actions { get; set; }
+        String rangeType { get; set; }
+
+        public AbstractAction getAction(string type)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Boolean isInRange(XRange range)
+        {
+            foreach (XRange nameRange in ranges)
+            {
+                if (nameRange.isInRange(range)>0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
